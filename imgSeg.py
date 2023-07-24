c66e620641c9d5dd0eb849da201a874e72350e5b
elkar_omar <omar.elkar.oe@gmail.com>
2023-07-24 10:16:57 +0100

    Update imgSeg.py

--- imgSeg.py
+++ imgSeg.py
@@ -68,57 +68,57 @@ def conv_neural_net(pretrained_weights = None, input_size = (512,512,1)):
     inputs = tf.keras.layers.Input(shape=input_size)
 
     # Convolutional Block 1
-    conv1 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer=tf.keras.initializers.he_normal())(inputs)
-    conv1 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer=tf.keras.initializers.he_normal())(conv1)
+    conv1 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer=tf.keras.initializers.he_normal)(inputs)
+    conv1 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer=tf.keras.initializers.he_normal)(conv1)
     pool1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(conv1)
 
     # Convolutional Block 2
     conv2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(pool1)
     conv2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv2)
-    conv2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(pool1)
-    conv2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv2)
+    conv2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(pool1)
+    conv2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv2)
     pool2 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(conv2)
 
     # Convolutional Block 3
-    conv3 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(pool2)
-    conv3 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv3)
+    conv3 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(pool2)
+    conv3 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv3)
     pool3 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(conv3)
 
     # Convolutional Block 4
-    conv4 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(pool3)
-    conv4 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv4)
+    conv4 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(pool3)
+    conv4 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv4)
     drop4 = tf.keras.layers.Dropout(0.5)(conv4)
     pool4 = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(drop4)
 
     # Convolutional Block 5
-    conv5 = tf.keras.layers.Conv2D(1024, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(pool4)
-    conv5 = tf.keras.layers.Conv2D(1024, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv5)
+    conv5 = tf.keras.layers.Conv2D(1024, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(pool4)
+    conv5 = tf.keras.layers.Conv2D(1024, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv5)
     drop5 = tf.keras.layers.Dropout(0.5)(conv5)
 
     # Upsampling Block 1
-    up6 = tf.keras.layers.Conv2D(512, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(tf.keras.layers.UpSampling2D(size=(2, 2))(drop5))
+    up6 = tf.keras.layers.Conv2D(512, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(tf.keras.layers.UpSampling2D(size=(2, 2))(drop5))
     merge6 = tf.keras.layers.Concatenate(axis=3)([drop4, up6])
-    conv6 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(merge6)
-    conv6 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv6)
+    conv6 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(merge6)
+    conv6 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv6)
 
     # Upsampling Block 2
-    up7 = tf.keras.layers.Conv2D(256, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(tf.keras.layers.UpSampling2D(size=(2, 2))(conv6))
+    up7 = tf.keras.layers.Conv2D(256, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(tf.keras.layers.UpSampling2D(size=(2, 2))(conv6))
     merge7 = tf.keras.layers.Concatenate(axis=3)([conv3, up7])
-    conv7 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(merge7)
-    conv7 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv7)
+    conv7 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(merge7)
+    conv7 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv7)
 
     # Upsampling Block 3
-    up8 = tf.keras.layers.Conv2D(128, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(tf.keras.layers.UpSampling2D(size=(2, 2))(conv7))
+    up8 = tf.keras.layers.Conv2D(128, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(tf.keras.layers.UpSampling2D(size=(2, 2))(conv7))
     merge8 = tf.keras.layers.Concatenate(axis=3)([conv2, up8])
-    conv8 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(merge8)
-    conv8 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv8)
+    conv8 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(merge8)
+    conv8 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv8)
 
     # Upsampling Block 4
-    up9 = tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(tf.keras.layers.UpSampling2D(size=(2, 2))(conv8))
+    up9 = tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(tf.keras.layers.UpSampling2D(size=(2, 2))(conv8))
     merge9 = tf.keras.layers.Concatenate(axis=3)([conv1, up9])
-    conv9 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(merge9)
-    conv9 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv9)
-    conv9 = tf.keras.layers.Conv2D(2, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal())(conv9)
+    conv9 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(merge9)
+    conv9 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv9)
+    conv9 = tf.keras.layers.Conv2D(2, (3, 3), activation='relu', padding='same', kernel_initializer=tf.keras.initializers.he_normal)(conv9)
     
     # Output layer
     conv10 = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(conv9)
